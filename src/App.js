b84ad207cc03fc09741d39335000ab3eb5fe08a7
Shinmt <shinminthwesmt99@gmail.com>
2023-05-09 18:49:31 +0630

    navbar_0509

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import Navbar from "./Navbar";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 import { Route, Routes} from "react-router-dom";
+import './App.css';
 
 function App() {
+
+  const [state, setState] = React.useState(false);
+
+  const helloStyle = {
+    backgroundColor : 'skyblue',
+    color : state ? 'white' : 'green',
+    height:"10vh",
+    width:"30vh",
+    textAlign:"center",
+  }
+
   return (
     <div>
       <Navbar />
@@ -14,6 +26,8 @@ function App() {
           <Route path="/details" element={<Details />} />
         </Routes>
       </div>
+      <h1 style={helloStyle}>Hello World!</h1>
+      <button onClick={()=> setState((prev) => !prev )}>Change textcolor</button>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom"
 export default function Navbar() {
+
     return <nav className="nav">
-        <Link to="/" className="site-name">ReactApp</Link>
+        <Link to="/" className="site-name" >ReactApp</Link>
         <ul>
             <li>
                 <Link to="/home">Home</Link>
